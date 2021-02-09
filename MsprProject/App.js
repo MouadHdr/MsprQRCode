1597e25bd62d23ca1396ca3a075321efca8e835e
ahammouti <ali.hammouti@epsi.fr>
2021-02-09 19:45:04 +0100

    change the city

--- MsprProject/App.js
+++ MsprProject/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! BERIZ</Text>
+      <Text>Open up App.js to start working on your app! joignyyyy</Text>
       <StatusBar style="auto" />
     </View>
   );
