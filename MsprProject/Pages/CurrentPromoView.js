c5a57a6e46e27659d34cbf4312c613452c232598
Mouad <mouad.hadirasso@gmail.com>
2021-02-24 17:47:30 +0100

    function test + cleaning

--- MsprProject/Pages/CurrentPromoView.js
+++ MsprProject/Pages/CurrentPromoView.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, SafeAreaView } from "react-native";
 
+import FonctionTest from "../Components/FonctionTest"
+
 export default function CurrentPromoView() {
+
   return (
     <SafeAreaView>
-      <Text>PAGE PROMO !!</Text>
+      <Text>PAGE PROMO oui !! <FonctionTest/> </Text>
 
       
     </SafeAreaView>
