13df772061211ed04445219351b34be8dfedc840
ahammouti <ali.hammouti@epsi.fr>
2021-03-02 11:14:38 +0100

    Read on DataBase : OK

--- MsprProject/Pages/CurrentPromoView.js
+++ MsprProject/Pages/CurrentPromoView.js
@@ -1,22 +1,59 @@
-import React from "react";
-import { Text, SafeAreaView, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, SafeAreaView, StyleSheet, ScrollView, View } from "react-native";
 import { Dimensions } from "react-native";
+import { ListItem } from 'react-native-elements';
 
 import ButtonGoBack from "../Components/ButtonGoBack";
+import firebaseConfig from '../config/FirebaseConfig';
 
 const { width, height } = Dimensions.get("window");
 
 export default function CurrentPromoView() {
-  
+
+  const [inPromo, setInPromo] = useState([])
+
+  useEffect(() => {
+    firebaseConfig.db.collection('InPromotions').onSnapshot(querySnapshot => {
+
+        const inPromo = [];
+
+        querySnapshot.docs.forEach((doc) => {
+          const {name, newPrice, oldPrice} = doc.data()
+          inPromo.push({
+              id: doc.id,
+              name,
+              newPrice,
+              oldPrice
+          })
+        });
+
+        setInPromo(inPromo)
+    });
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>PAGE PROMO !</Text>
+    <View >
+      <Text style={{ position: 'absolute', width:"100%",textAlign:"center", marginTop: 50}}>PAGE PROMO !!</Text>
+      <ScrollView style={{marginTop:80, marginBottom:90}}>
+        {
+            inPromo.map(inpromo => {
+              return (
+                  <ListItem style={{ maxWidth:"90%", marginLeft:15 ,borderWidth:0.8, borderColor:"#591259"}} key={inpromo.id}>
+                      <ListItem.Content >
+                        <ListItem.Title>{inpromo.name} </ListItem.Title>
+                        <ListItem.Subtitle>{inpromo.newPrice + '€' + ' ' + '-' + ' ' + inpromo.oldPrice + '€'}</ListItem.Subtitle>
+                      </ListItem.Content>
+                  </ListItem>
+              )
+            })
+        }
+      </ScrollView>
       <View style={styles.containerButtonGoBack}>
         <View style={styles.ButtonGoBack}>
           <ButtonGoBack />
         </View>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
 
